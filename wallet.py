011943e385a94df4c5571bcf32b9efcdb69b6efa
William Chance <william@chance.tel>
2021-07-14 19:20:34 -0400

    Started but tough

---
+++ wallet.py
@@ -0,0 +1,34 @@
+# Import dependencies
+import subprocess
+import json
+from dotenv import load_dotenv
+
+# Load and set environment variables
+load_dotenv('mnemonic.env')
+mnemonic=os.getenv("mnemonic")
+
+# Import constants.py and necessary functions from bit and web3
+from constants import *
+  
+# Create a function called `derive_wallets`
+def derive_wallets():
+    command = 'php ./derive --mnemonic="{mnemonic}" --coin=btc --numderive=3 --format=json'
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    output, err = p.communicate()
+    p_status = p.wait()
+    return json.loads(output)
+
+# Create a dictionary object called coins to store the output from `derive_wallets`.
+#coins = # YOUR CODE HERE
+
+# Create a function called `priv_key_to_account` that converts privkey strings to account objects.
+#def priv_key_to_account(# YOUR CODE HERE):
+    # YOUR CODE HERE
+
+# Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
+#def create_tx(# YOUR CODE HERE):
+    # YOUR CODE HERE
+
+# Create a function called `send_tx` that calls `create_tx`, signs and sends the transaction.
+#def send_tx(# YOUR CODE HERE):
+    # YOUR CODE HERE
