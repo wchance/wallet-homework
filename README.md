88f5365599402eb30794ab2f043140719bcfe688
William Chance <william@chance.tel>
2021-07-15 01:42:57 -0400

    Trying to finish

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# wallet-homework
+# Multi-Blockchain Wallet in Python
+
+### Enviroment setup
+- Step 1: Install HD Wallet Derive
+- Step 2: Install web3 and bit
--- wallet.py
+++ wallet.py
@@ -1,6 +1,7 @@
 # Import dependencies
 import subprocess
 import json
+from bit.network.services import NetworkAPI
 from dotenv import load_dotenv
 import os
 
@@ -41,14 +42,37 @@ def priv_key_to_account(coin, priv_key):
     return None
 
 # Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
-#def create_tx(coin, account, to, amount):
-    #if coin == ETH: 
+def create_tx(coin, account, to, amount):
+    if coin == ETH:
+        gasEstimate = w3.eth.estimateGas(
+            {"from": account.address, "to": to, "value": amount}
+        )
+        return {
+            "to": to,
+            "from": account.address,
+            "value": amount,
+            "gas": gasEstimate,
+            "gasPrice": w3.eth.gasPrice,
+            "nonce": w3.eth.getTransactionCount(account.address),
+            "chainID": 1337
+        }
+    elif coin == BTCTEST:
+        return PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTC)])
 
 # Create a function called `send_tx` that calls `create_tx`, signs and sends the transaction.
-#def send_tx(coin, account, to, amount):
-    #txn = create_tx(coin, account, to, amount)
+def send_tx(coin, account, to, amount):
+    tx = create_tx(coin, account, to, amount)
+    if coin == ETH:
+        tx_signed = account.sign_transaction(tx)
+        result = w3.eth.send_raw_transaction(tx_signed.rawTransaction)
+        return results.hex()
+    elif coin == BTCTEST:
+        btctest_tx = create_tx(coin, account, to, amount)
+        tx_signed = account.sign_transaction(tx)
+        return NetworkAPI.broadcast_tx_testnet(tx_signed)
 
-print(priv_key_to_account(ETH, '0x526ba7ad5543dbf0f8163f99966b7d43cedd49e9f0421ea8b45e76009c4e785b'))
+
+#print(priv_key_to_account(ETH, '0x526ba7ad5543dbf0f8163f99966b7d43cedd49e9f0421ea8b45e76009c4e785b'))
 
 #WORKS
 #print(priv_key_to_account(BTCTEST, 'cRH5aKMLYrjNBkeFXBHmVyzjSR1wqfwMnxEh5QYyu9o2QiRtyFw8'))
